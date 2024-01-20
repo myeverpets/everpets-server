fe55a4013c642fb0c998c24f30ae0e8cd204f958
Zakhar Ilchuk <zakharilchuk1@gmail.com>
2024-01-20 13:34:19 +0200

    Added sms notification service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { MailerModule } from './mailer/mailer.module';
+import { SnsModule } from './sns/sns.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { MailerModule } from './mailer/mailer.module';
     UserModule,
     AuthModule,
     MailerModule,
+    SnsModule,
   ],
   controllers: [],
   providers: [],
